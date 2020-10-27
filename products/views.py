7b052fbc955f96e6a991060062db2ca0e735ab9c
Bassam-home <aljeedjedbs@gmail.com>
2020-10-27 16:21:34 +0300

    to make the temlate

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Product
+
+# Create your views here.
+def products_list(request):
+    products = Product.objects.all()
+    return  render(request , 'product/products-list.html' , {'products':products})
+
+
+def product_details(request , pk):
+    product = get_object_or_404(Product ,pk=pk)
+    return render (request , 'product/product-details.html' , {'product': product})
+
+
